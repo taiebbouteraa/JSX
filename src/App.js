ebfd5f388bf81aaecfd8bfc1e2d1f101f697f51e
taiebbouteraa <bouteraa.taieb@gmail.com>
2021-06-10 18:00:18 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import './Style.css';
+import img from './imageInSrc.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <div style={{border:"solid 1px black", maxWidth:"100vw"}}>
+      <h1 className="title red">Mohamed Taieb Bouteraa</h1>
+      <br />
+      <img src={img} alt="img1" />
+      <br />
+      <img src="./imageInPublic.jpg" alt="img2"/>
     </div>
+    <video width="320" height="240" controls >
+      <source src="myVideo.mp4" type="video/mp4" />
+    </video>
+  </>
   );
 }
 
